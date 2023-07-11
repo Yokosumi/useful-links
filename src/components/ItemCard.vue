6dbd3e4bfd31ee690e3940db1fe6fc845474f246
Selman Ciftci <selman.ciftci.coding@gmail.com>
2023-07-11 20:09:44 +0200

    changed minor style

--- src/components/ItemCard.vue
+++ src/components/ItemCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="card w-96 bg-base-100 shadow-xl">
+  <div class="card w-96 bg-base-100 shadow-xl mt-3">
     <figure><img src="../public/imges/lordicon.JPG" alt="Shoes" /></figure>
     <div class="card-body">
       <h2 class="card-title">
