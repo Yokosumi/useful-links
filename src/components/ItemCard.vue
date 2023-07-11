692e445a089e0de929c204cd33e512527644995e
Selman Ciftci <selman.ciftci.coding@gmail.com>
2023-07-11 21:05:54 +0200

    tried to fix json render issue

--- src/components/ItemCard.vue
+++ src/components/ItemCard.vue
@@ -1,14 +1,28 @@
 <template>
   <div class="card w-96 bg-base-100 shadow-xl mt-3">
-    <figure><img src="../public/imges/lordicon.JPG" alt="Shoes" /></figure>
+    <figure>
+      <a href="#"><img src="../public/images/lordicon.JPG" alt="preview" /> </a>
+    </figure>
     <div class="card-body">
       <h2 class="card-title">
-        lordicons
+        {{ jasonData.name }}
         <div class="badge badge-secondary">Icons</div>
         <div class="badge badge-primary">Animated</div>
       </h2>
-      <p>If a dog chews shoes whose shoes does he choose?</p>
+      <p>If a dog chews shoes, whose shoes does he choose?</p>
       <div class="card-actions justify-end"></div>
     </div>
   </div>
 </template>
+
+<script>
+import json from '../data/data.json'
+
+export default {
+  data() {
+    return {
+      jasonData: json
+    }
+  }
+}
+</script>
