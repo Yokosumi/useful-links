fad07d4a5737d051978a0491a182fde78e5148da
Selman Ciftci <selman.ciftci.coding@gmail.com>
2023-07-11 19:31:07 +0200

    fixed itemcard component

--- src/views/GeneratorsView.vue
+++ src/views/GeneratorsView.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
-    <h1>This is an generators page</h1>
+    <h1>This is a generators page</h1>
   </div>
   <item-card></item-card>
 </template>
+
+<script>
+import ItemCard from '../components/ItemCard.vue'
+
+export default {
+  components: {
+    ItemCard
+  }
+}
+</script>
