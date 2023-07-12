7cde25b969d2310255b5c1fb07b27551cc546e7b
Selman Ciftci <selman.ciftci.coding@gmail.com>
2023-07-12 11:30:04 +0200

    parsing .json data works

--- src/components/ItemCard.vue
+++ src/components/ItemCard.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="card w-96 bg-base-100 shadow-xl mt-3">
-    <figure>
+    <figure class="bg-white">
       <a :href="jasonData[0].url"><img :src="jasonData[0].image" alt="preview" /> </a>
     </figure>
     <div class="card-body">
       <h2 class="card-title">
         {{ jasonData[0].name }}
-        <div class="badge badge-secondary">Icons</div>
-        <div class="badge badge-primary">Animated</div>
+        <div class="badge badge-secondary">{{ jasonData[0].firstBadge }}</div>
+        <div class="badge badge-primary">{{ jasonData[0].secondBadge }}</div>
       </h2>
-      <p>If a dog chews shoes, whose shoes does he choose?</p>
+      <p>
+        {{ jasonData[0].description }}
+      </p>
       <div class="card-actions justify-end"></div>
     </div>
   </div>
